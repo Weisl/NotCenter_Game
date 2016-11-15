b39154d61d04aad4328a6e1817e735388b362103
subcultur3 <e.glaubauf@gmail.com>
2016-11-15 09:28:48 +0100

    Schaun dass die info übertragen wird

---
+++ Assets/keepAlive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class keepAlive : MonoBehaviour {
+
+
+    public GameObject alive;
+
+    void Awake()
+    {
+              
+        DontDestroyOnLoad(alive);
+    }
+
+   
+}
